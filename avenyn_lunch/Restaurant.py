e73803dcfadc8c6d1d1eb46923ac8530d2777845
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-04-09 16:13:06 +0200

    fewer lines

--- avenyn_lunch/Restaurant.py
+++ avenyn_lunch/Restaurant.py
@@ -3,13 +3,7 @@ class Restaurant(object):
     def __init__(self, name, image):
         self.name = name
         self.image = image
-        self.weekly_menu = [
-            [],
-            [],
-            [],
-            [],
-            []
-        ]
+        self.weekly_menu = [[], [], [], [], []]
 
     def add_menu_item(self, dayname, food):
         self.weekly_menu[dayname].append(food)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='avenyn-lunch',
-    version='1.4',
+    version='1.5',
     author='ianertson',
     author_email='ianertson@gmail.com',
     install_requires=[
